971065289af1cad13accc750a7130f20ab167bdd
Aghiles Zaidi <aghiles@agence-lt.fr>
2021-04-29 14:44:33 +0200

    modif sayensPlugin.php

--- SayensPlugin/sayensPlugin.php
+++ SayensPlugin/sayensPlugin.php
@@ -1,35 +1,35 @@
 <?php
-/**
- * Plugin Name: Plugin Sayens
- */
-
-// Create the API client with the main uri and the version of the API
-ClientApi clientApi = new ClientApi("http://crm.sayens.fr/cxm/rest", "v2");
-// Set the api key
-clientApi.setApiKey("d0de349aa995a7e42e6169f25f2bbd0e");
-
-// creer une fonction pour recupérer les input du formuaire
-function get_form_input() {
-
-        $data = json_encode(array(
-        "code"  => "string",
-        "companyFree" => "string",
-        "address" => "string",
-        "contactFree" => "string",
-        "contactFunction" => "string",
-        "phone" => "string",
-        "email" => "string",
-        ));
-
-        $ch = curl_init(); 
-        curl_setopt($ch, CURLOPT_URL,$url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        $output = curl_exec($ch);
-        curl_close($ch);
-    }  
-    
-    
 
 
-add_action('get_form_input');
+$key = "d0de349aa995a7e42e6169f25f2bbd0e";
+
+$url = "http://crm.sayens.fr/cxm/rest/leads";
+
+$url = $url . $key;
+
+
+
+$data = json_encode(array(
+    "code" => "formulaire inscription event",
+    "companyFree" => "Nike incorporation",
+    "address" => "10 rue des Pâquerettes, Nancy 54000",
+    "contactFree" => "Gwennael Deronne",
+    "contactFunction" => "développeur",
+    "phone" => "0781144976",
+    "email" => "contact@free.fr",
+));
+
+$ch = curl_init(); 
+curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_POST, $data);
+$output = curl_exec($ch);
+curl_close($ch);
+
+add_filter('auto_update_plugin', '__return_false');
+add_filter('plugin_auto_update_enabled', '__return_false');
+add_filter('theme_auto_update_enabled', '__return_false');
+add_filter('allow_major_auto_core_update', '__return_false');
+
+var_dump($output);
